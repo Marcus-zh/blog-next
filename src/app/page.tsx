69eca7035334573ab42ebbe1d788673809a99bf0
Marcus <marcus-zh@qq.com>
2024-07-09 21:28:25 +0800

    test: update getPostByField function to retrieve posts by '总结' tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { getPostByField } from "@/core/posts";
 
 export default function Home() {
-  const result = getPostByField('tags', 'Hexo');
+  const result = getPostByField('tags', '总结');
   console.log("Result:", result);
 }
