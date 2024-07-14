1de0483eb5e91f75f3e0e944bfdcdde5ddf1fd4b
Marcus <marcus-zh@qq.com>
2024-07-14 12:10:24 +0800

    feat: Add email functionality for sending mail

---
+++ src/core/email.ts
@@ -0,0 +1,22 @@
+import { render } from '@react-email/components';
+import nodemailer from 'nodemailer';
+
+export const sender = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+export function getMailOpinion(to: string, subject: string,emailHtml: string) {
+  return {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    html: emailHtml,
+  };
+}
+
