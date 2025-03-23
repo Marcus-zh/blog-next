ca25d23c6d3241d1c742fcee078cf11ec621b39e
Marcus <marcus-zh@qq.com>
2025-03-23 11:59:54 +0800

    fix🐛: remove unused imports in email

--- src/core/email.ts
+++ src/core/email.ts
@@ -1,29 +1,27 @@
-import { render } from '@react-email/components';
-import nodemailer from 'nodemailer';
-// import { getAllFriends } from './friends';
-// import { Friend } from '@/interface/friends';
+// import { render } from '@react-email/components';
+// import nodemailer from 'nodemailer';
 
-export const sender = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-});
+// export const sender = nodemailer.createTransport({
+//   host: process.env.EMAIL_HOST,
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASSWORD,
+//   },
+// });
 
-export function getMailOpinion(to: string, subject: string,emailHtml: string) {
-  return {
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    html: emailHtml,
-  };
-}
-
-// export async function getAllEmail(){
-//   const allFriends = await getAllFriends()
-//   const emailList = allFriends.map((friend: Friend) => friend.acceptPush && friend.email)
-//   return emailList
+// export function getMailOpinion(to: string, subject: string,emailHtml: string) {
+//   return {
+//     from: process.env.EMAIL_USER,
+//     to,
+//     subject,
+//     html: emailHtml,
+//   };
 // }
+
+// // export async function getAllEmail(){
+// //   const allFriends = await getAllFriends()
+// //   const emailList = allFriends.map((friend: Friend) => friend.acceptPush && friend.email)
+// //   return emailList
+// // }
